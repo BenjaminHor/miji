8871ec795ce9804a2b4ae9ceb65e94e48f16851a
Ben Hor <rohmin@MacBook-Pro.local>
2022-06-01 20:49:59 -0400

    Saving theme to local storage

--- src/GameManager.svelte
+++ src/GameManager.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 import BoardView from "./components/BoardView.svelte";
 import { BoardModel } from "./models/board_model";
-import { tile_catalog, color_palettes, config, current_theme } from "./store"
+import { tile_catalog, color_palettes, config } from "./store"
 
 let board_model = new BoardModel($config.board_size, $config.board_size)
 
@@ -16,7 +16,7 @@ let tiles = fetch("data/tiles.json")
 let colors = fetch("data/color_palettes.json")
 	.then(response => response.json())
 	.then(json => $color_palettes = json)
-	.then(() => change_theme("8008"))
+	.then(() => set_theme(null))
 
 function change_board_size(delta: number) {
 	board_model.change_board_size(delta)
@@ -24,12 +24,26 @@ function change_board_size(delta: number) {
 	board_model = board_model
 }
 
-function change_theme(theme: string) {
-	$current_theme = $color_palettes[theme]
-	document.documentElement.style.setProperty("--bg-color", $current_theme["bg-color"])
-	document.documentElement.style.setProperty("--main-color", $current_theme["main-color"])
-	document.documentElement.style.setProperty("--sub-color", $current_theme["sub-color"])
-	document.documentElement.style.setProperty("--text-color", $current_theme["text-color"])
+function set_theme(theme: string) {
+	let chosen_theme = "8008"
+
+	if (theme == null) {
+		// Check local storage for a saved theme
+		let saved_theme = window.localStorage.getItem('current_theme')
+		if (saved_theme != null) {
+			chosen_theme = saved_theme
+		}
+	} else {
+		chosen_theme = theme
+	}
+
+	$config.current_theme = $color_palettes[chosen_theme]
+	$config.current_theme_name = chosen_theme
+	window.localStorage.setItem('current_theme', chosen_theme)
+	document.documentElement.style.setProperty("--bg-color", $config.current_theme["bg-color"])
+	document.documentElement.style.setProperty("--main-color", $config.current_theme["main-color"])
+	document.documentElement.style.setProperty("--sub-color", $config.current_theme["sub-color"])
+	document.documentElement.style.setProperty("--text-color", $config.current_theme["text-color"])
 }
 
 </script>
@@ -42,9 +56,13 @@ function change_theme(theme: string) {
 		<button on:click={() => change_board_size(-1)}>-</button>
 		<button on:click={() => change_board_size(0)}>New</button>
 		<div>
-			<select name="themes" on:change={event => change_theme(event.target.value)}>
+			<select name="themes" on:change={event => set_theme(event.target.value)}>
 				{#each Object.keys($color_palettes) as theme}
-					<option value={theme}>{theme}</option>
+					{#if theme == $config.current_theme_name}
+						<option selected value={theme}>{theme}</option>
+					{:else}
+						<option value={theme}>{theme}</option>
+					{/if}
 				{/each}
 			</select>
 		</div>
