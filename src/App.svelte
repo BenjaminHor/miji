7fbeff061a9d9711eb596a7b68c853c9937dffc4
Benjamin Hor <benjaminvhor@gmail.com>
2022-12-19 00:28:15 -0500

    cleaning up html in TileView

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
-import { slide } from "svelte/types/runtime/transition";
-
 import TileView from "./components/TileView.svelte";
-
 import { Rotation, TileModel, Side } from "./models/tile_model";
 
 let tile = TileModel.from_array(["1ab3", "7dc4"])
 </script>
 
-<TileView tile_model={tile}/>
+<div>
+	<TileView tile_model={tile}/>
+</div>
 
 <style>
 </style>
