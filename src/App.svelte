2e5634658edb9cfd69b88829bcf3aec3e18f3b9a
Benjamin Hor <benjaminvhor@gmail.com>
2022-12-19 00:28:15 -0500

    initial rotation functionality for tiles

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
 import TileView from "./components/TileView.svelte";
 import { Rotation, TileModel, Side } from "./models/tile_model";
 
-let tile = TileModel.from_array(["1ab3", "7dc4"])
+let tile = TileModel.from_array(["1abcd7"])
 </script>
 
 <div>
